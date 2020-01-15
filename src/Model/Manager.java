d374b66bd137244b83302ea1c3141f8fbd8f5f21
nivgold <nivgold@post.bgu.ac.il>
2020-01-15 17:05:56 +0200

    part b version 8 - final

--- src/Model/Manager.java
+++ src/Model/Manager.java
@@ -267,6 +267,7 @@ public class Manager {
         Parse.docCounter.set(0);
         Parse.totalDocLength.set(0);
         Indexer.invertedIndexDictionary = new ConcurrentHashMap<>();
+
     }
 
     /**
